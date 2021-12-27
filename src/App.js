7faffd28fd8acf4af444f591a5c867dd9303cc71
Henry Betancourth <henrybetancourth@Henrys-Mac-mini.fios-router.home>
2021-12-26 20:23:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext, useEffect } from 'react';
+import SetPomodoro from './components/SetPomodoro';
+import { SettingContext } from './context/SettingsContext';
+import CountdownAnimation from './components/CountdownAnimation';
+import Button from './components/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const {
+		pomodoro,
+		executing,
+		startAnimate,
+		children,
+		startTimer,
+		pauseTimer,
+		updateExecute,
+		setCurrentTimer,
+		SettingsBtn,
+	} = useContext(SettingContext);
+
+	useEffect(() => {
+		updateExecute(executing);
+	}, [executing, startAnimate]);
+
+	return (
+		<div className="container">
+			<h1>Pomodoro</h1>
+			<small>Be productive the right way.</small>
+			{pomodoro !== 0 ? (
+				<>
+					<ul className="labels">
+						<li>
+							<Button
+								title="Work"
+								activeClass={
+									executing.active === 'work' ? 'active-label' : undefined
+								}
+								_callback={() => setCurrentTimer('work')}
+							/>
+						</li>
+						<li>
+							<Button
+								title="Short Break"
+								activeClass={
+									executing.active === 'short' ? 'active-label' : undefined
+								}
+								_callback={() => setCurrentTimer('short')}
+							/>
+						</li>
+						<li>
+							<Button
+								title="Long Break"
+								activeClass={
+									executing.active === 'long' ? 'active-label' : undefined
+								}
+								_callback={() => setCurrentTimer('long')}
+							/>
+						</li>
+					</ul>
+					<Button title="Settings" _callback={SettingsBtn} />
+					<div className="timer-container">
+						<div className="time-wrapper">
+							<CountdownAnimation
+								key={pomodoro}
+								timer={pomodoro}
+								animate={startAnimate}>
+								{children}
+							</CountdownAnimation>
+						</div>
+					</div>
+					<div className="button-wrapper">
+						<Button
+							title="Start"
+							activeClass={!startAnimate ? 'active' : undefined}
+							_callback={startTimer}
+						/>
+						<Button
+							title="Pause"
+							activeClass={startAnimate ? 'active' : undefined}
+							_callback={pauseTimer}
+						/>
+					</div>
+				</>
+			) : (
+				<SetPomodoro />
+			)}
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/CountdownAnimation.js
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { SettingContext } from '../context/SettingsContext';
+
+const CountdownAnimation = ({ key = 1, timer = 20, animate = true, children }) => {
+	const { stopTimer } = useContext(SettingContext);
+
+	return (
+		<CountdownCircleTimer
+			key={key}
+			isPlaying={animate}
+			duration={timer * 60}
+			colors={[
+				['#FE6F6B', 0.33],
+				['#FE6F6B', 0.33],
+				['#FE6F6B', 0.33],
+			]}
+			strokeWidth={6}
+			size={220}
+			trailColor="#151932"
+			onComplete={() => {
+				stopTimer();
+			}}>
+			{children}
+		</CountdownCircleTimer>
+	);
+};
+
+export default CountdownAnimation;
